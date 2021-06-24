ab8a9d8659253ce52848b44e7e57c722f6cc91ae
soa-soa <you@example.com>
2021-06-24 11:47:53 -0700

    progress commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index. This is from the hth_django_tutorial folder.")
+
+def detail(request, question_id):
+    return HttpResponse("Your're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "Your're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
