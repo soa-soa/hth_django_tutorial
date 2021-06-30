6607bcb8263ef282cb9367f0bede14748b8ddc22
soa-soa <you@example.com>
2021-06-30 11:49:04 -0700

    lab 9

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
