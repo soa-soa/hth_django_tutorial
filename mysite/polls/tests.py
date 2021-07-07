a247a1815c13eaedebfaff245f2a590de63e057a
soa-soa <sophialhuang@gmail.com>
2021-07-07 11:45:51 -0700

    part 6: created stylesheet

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -71,8 +71,7 @@ class QuestionIndexViewTests(TestCase):
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
-
-    self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_future_question_and_past_question(self):
         """
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -53,4 +53,3 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-        
